f4e017f00250ecff69e157b4386574254ac05331
Ketan Rathod <ketan.rathod@nexuslinkservices.in>
2023-01-03 15:24:30 +0530

    save and reset button in class added.

--- src/Form/TaskType.php
+++ src/Form/TaskType.php
@@ -7,6 +7,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\ResetType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
@@ -23,7 +24,12 @@ class TaskType extends AbstractType
                 'help' => 'task description',
                 'attr' => ['class' => 'form-control']
             ])
-            ->add('save', SubmitType::class)
+            ->add('save', SubmitType::class,[
+                'attr' => ['class' => 'btn btn-success']
+            ])
+            ->add('reset', ResetType::class,[
+                'attr' => ['class' => 'btn btn-info']
+            ])
         ;
     }
 
